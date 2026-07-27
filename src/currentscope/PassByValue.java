dd88ac9f4d30db8851abb6ddea56b3d62e199d5f
wilcywilson <wilsonshresthaapps@gmail.com>
2026-07-27 18:18:01 +0545

    Arrays and Object Wrapper Pass By Value

--- src/currentscope/PassByValue.java
+++ src/currentscope/PassByValue.java
@@ -1,5 +1,7 @@
 package currentscope;
 
+import java.util.Arrays;
+
 public class PassByValue {
     private int test = 1000;
 
@@ -36,7 +38,7 @@ public class PassByValue {
         System.out.println(System.identityHashCode(passByValue1));
         System.out.println(System.identityHashCode(passByValue2));
 
-        System.out.printf("\nLocal Mutated Values after swap: \n");
+        System.out.println("\nLocal Mutated Values after swap: \n");
         System.out.println("\nPassByValue1 Mutated");
         passByValue1.getMutatedObject();
         System.out.println("\nPassByValue2 Mutated");
@@ -49,6 +51,29 @@ public class PassByValue {
 
     void getMutatedObject() {
         System.out.println(test);
+    }
+
+    // Object Wrapper act like Primitive Type and won't swap the actual value outside the method
+    void objectWrapper(String x, Integer y) {
+        System.out.println("\nInside Method: Object Wrapper before swap");
+        System.out.println(x);
+        System.out.println(y);
+        x = x + "What";
+        y = y + 10;
+        System.out.println("\nInside Method: Object Wrapper after swap");
+        System.out.println(x);
+        System.out.println(y);
+    }
+
+    // Arrays are references too
+    void arrayTest(int[] arr) {
+        System.out.println("\nInside Method: Array before swap");
+        System.out.println(Arrays.toString(arr));
+        arr[0] = 99; // mutation is visible to caller
+        System.out.println("\nInside Method: Array after swap");
+        System.out.println(Arrays.toString(arr));
+        arr = new int[]{1, 2}; // reassignment is not visible because it's pointing to new memory reference now
+        System.out.println(Arrays.toString(arr));
     }
 
     public static void main(String[] args) {
@@ -77,5 +102,22 @@ public class PassByValue {
         passByValue1.getMutatedObject();
         System.out.println("\nPassByValue2 Mutated");
         passByValue2.getMutatedObject();
+
+        System.out.println("\nObject Wrapper before swap");
+        String value1 = "abc";
+        Integer value2 = 20;
+        System.out.println(value1);
+        System.out.println(value2);
+        passByValue.objectWrapper(value1, value2);
+        System.out.println("\nObject Wrapper after swap");
+        System.out.println(value1);
+        System.out.println(value2);
+
+        int[] arr = new int[]{20};
+        System.out.println("\nOutside Method: Array before swap");
+        System.out.println(Arrays.toString(arr));
+        passByValue.arrayTest(arr);
+        System.out.println("\nOutside Method: Array after swap");
+        System.out.println(Arrays.toString(arr));
     }
 }

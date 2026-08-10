8ccb9bbb1bdcdc7ca41499076aedf10eb3a396e5
wilcywilson <wilsonshresthaapps@gmail.com>
2026-08-10 18:56:03 +0545

    Generic Method with Bounds

--- src/javaprepfirstscope/GenericMethodsDemo.java
+++ src/javaprepfirstscope/GenericMethodsDemo.java
@@ -1,6 +1,6 @@
 package javaprepfirstscope;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class GenericMethodsDemo {
     private final Integer value1;
@@ -14,14 +14,44 @@ public class GenericMethodsDemo {
     // Generic Method with Single Type
     public static <T> void printArray(T[] arr) {
         // containingObject::instanceMethodName
-        Arrays.stream(arr).forEach(System.out::print);
+        Arrays.stream(arr).forEach(element -> System.out.print(element + " "));
 
+        System.out.println();
         // Consumer Generic accepts a single input argument of type T and returns no result (void)
         // Arrays.stream(arr).forEach(input -> System.out.println(input));
     }
 
+    // Generic Method returning the type
+    public static <T> T getFirst(T[] arr) {
+        var stringArr = new ArrayList<String>();
+        System.out.println();
+        Arrays.stream(arr).forEach(element -> stringArr.add(element.toString()));
+        stringArr.forEach(System.out::println);
+        return arr.length > 0 ? arr[0] : null;
+    }
+
+    // Generic method with multiple bounds
+    // T must be a class/object that implements the Comparable interface
+    public static <T extends Comparable<T>> T findMax(T[] array) {
+        if (array == null || array.length == 0) {
+            return null;
+//             Dereference of 'array' may produce 'NullPointerException'
+//            return array.length;
+        }
+
+        return Arrays.stream(array).filter(Objects::nonNull).max(Comparator.naturalOrder()).orElse(null);
+    }
+
     public static void main(String[] args) {
-        var arr = new Integer[]{1, 2, 3, 4, 5};
+        var arr = new Integer[]{1, 2, 3, 4, 555, 55, 123, 222, 101};
+        var arr2 = new Integer[20];
         printArray(arr);
+
+        System.out.println("\n" + getFirst(arr) + "\n");
+
+        // Find Max
+        System.out.println(findMax(arr));
+
+        System.out.println(findMax(arr2));
     }
 }

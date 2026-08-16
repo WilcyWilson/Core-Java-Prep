e6b506b423943e26ff21604d04aad3c895197ea7
wilcywilson <wilsonshresthaapps@gmail.com>
2026-08-16 14:35:16 +0545

    Leetcode Twosum using Hashmap and Streams

--- src/leetcodeprep/TwoSum.java
+++ src/leetcodeprep/TwoSum.java
@@ -1,25 +1,60 @@
 package leetcodeprep;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.IntStream;
+
 public class TwoSum {
     public static void main(String[] args) {
-        int[] nums = new int[]{3,3};
-        int target = 6;
+        int[] nums = new int[]{2, 7, 11, 15};
+        int target = 18;
         TwoSum twoSum = new TwoSum();
-        System.out.println("[" + twoSum.twoSum(nums,target)[0] + "," + twoSum.twoSum(nums,target)[1] + "]");
+        int[] result = twoSum.twoSum(nums, target);
+
+        System.out.println("[" + result[0] + "," + result[1] + "]");
+
+        result = twoSum.twoSumHashMap(nums, target);
+        System.out.println("[" + result[0] + "," + result[1] + "]");
+        result = twoSum.twoSumStream(nums, target);
+        System.out.println("[" + result[0] + "," + result[1] + "]");
     }
 
+    // Higher time complexity, lower space complexity. Time Complexity - O(n) x O(n) = O (n²)
     public int[] twoSum(int[] nums, int target) {
-        int[] result = new int[2];
-        outerLoop: // Using label to break out of two loops at once
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {
-                    result[0] = i;
-                    result[1] = j;
-                    break outerLoop;
+                    return new int[]{i, j};
                 }
             }
         }
-        return result;
+        return new int[]{0, 0};
+    }
+
+    // Higher time complexity, lower space complexity. Time Complexity - O(n) x O(n) = O (n²)
+    // Super slow due to boxing and unboxing
+    public int[] twoSumStream(int[] nums, int target) {
+
+        return IntStream.range(0, nums.length).
+                boxed().
+                flatMap(i ->
+                        IntStream.range(i + 1, nums.length).
+                                filter(j -> nums[i] + nums[j] == target).
+                                mapToObj(j -> new int[]{i, j})).
+                findFirst().
+                orElse(new int[]{0, 0});
+    }
+
+    // Lower time complexity, higher space complexity. Time Complexity - O(1) x O(n) = O (n)
+    public int[] twoSumHashMap(int[] nums, int target) {
+        Map<Integer, Integer> numsHashMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int searchInt = target - nums[i];
+            if (numsHashMap.containsKey(searchInt)) {
+                return new int[]{numsHashMap.get(searchInt), i};
+            }
+            numsHashMap.put(nums[i], i);
+        }
+        return new int[]{0, 0};
     }
 }

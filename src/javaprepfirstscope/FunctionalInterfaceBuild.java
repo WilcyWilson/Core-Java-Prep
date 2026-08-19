980606aa5332a8f3147e2f95d390be09883fac93
wilcywilson <wilsonshresthaapps@gmail.com>
2026-08-19 12:02:40 +0545

    Custom map Implementation

--- src/javaprepfirstscope/FunctionalInterfaceBuild.java
+++ src/javaprepfirstscope/FunctionalInterfaceBuild.java
@@ -1,14 +1,17 @@
 package javaprepfirstscope;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class FunctionalInterfaceBuild {
 
-    public <T> List<T> filterCustom(List<T> listOfValues, Predicate<T> condition) {
+    public static final int HALF_POWER = 50;
+    public static final int POWER_INCREASE_THRESHOLD = 10;
+    public static final int FULL_POWER = 100;
+
+    public static <T> List<T> filterCustom(List<T> listOfValues, Predicate<T> condition) {
         List<T> resultListOfValues = new ArrayList<>();
         for (T value : listOfValues) {
             if (condition.test(value)) {
@@ -18,25 +21,36 @@ public class FunctionalInterfaceBuild {
         return resultListOfValues;
     }
 
-    public <T> void forEachCustom(List<T> listOfValues, Consumer<T> action) {
+    public static <T> void forEachCustom(List<T> listOfValues, Consumer<T> action) {
         for (T value : listOfValues) {
             action.accept(value);
         }
     }
 
+    public static <T, R> List<R> mapCustom(List<T> listOfValues, Function<T, R> transformer) {
+        List<R> returnValue = new ArrayList<>();
+        for (T value : listOfValues) {
+            returnValue.add(transformer.apply(value));
+        }
+        return returnValue;
+    }
+
     public static void main(String[] args) {
-        List<String> heroes = new ArrayList<>(Arrays.asList("Spider", "Platic Man", "Ironman", "Batman", "Superman", "Batman 2099"));
-        new FunctionalInterfaceBuild().filterCustom(heroes, hero -> !hero.contains("Batman")).forEach(System.out::println);
+        List<String> heroes = new ArrayList<>(Arrays.asList("Spider", "Plastic Man", "Ironman", "Batman", "Superman", "Batman 2099"));
+        filterCustom(heroes, hero -> !hero.contains("Batman")).forEach(System.out::println);
 
         System.out.println();
-        List<Integer> powerLevels = new ArrayList<>(Arrays.asList(80, 90, 50, 20, 100));
-        new FunctionalInterfaceBuild().filterCustom(powerLevels, power -> power > 50).forEach(System.out::println);
+        List<Integer> powerLevels = new ArrayList<>(Arrays.asList(80, 90, 50, 20, 100, 70));
+        filterCustom(powerLevels, power -> power > HALF_POWER).forEach(System.out::println);
 
         System.out.println();
-        new FunctionalInterfaceBuild().forEachCustom(heroes, System.out::println);
+        forEachCustom(heroes, System.out::println);
 
         System.out.println();
-        new FunctionalInterfaceBuild().forEachCustom(powerLevels, power -> System.out.println(power + 10 > 100 ? power : power + 10));
+        forEachCustom(powerLevels, power -> System.out.println(power + POWER_INCREASE_THRESHOLD > FULL_POWER ? power : power + POWER_INCREASE_THRESHOLD));
+
+        System.out.println();
+        mapCustom(heroes, String::length).forEach(System.out::println);
     }
 
 }

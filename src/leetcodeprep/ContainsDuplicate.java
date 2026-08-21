6515d898af2aa00fe865392d4ef2f2df719378c6
wilcywilson <wilsonshresthaapps@gmail.com>
2026-08-21 10:33:59 +0545

    ContainsDuplicate Solutions

---
+++ src/leetcodeprep/ContainsDuplicate.java
@@ -0,0 +1,74 @@
+package leetcodeprep;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ContainsDuplicate {
+
+    // TLE
+    public boolean containsDuplicateLoops(int[] nums) {
+        int[] numsCopy = new int[nums.length];
+        System.arraycopy(nums, 0, numsCopy, 0, nums.length);
+        int last = nums.length - 1;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (numsCopy[j] == numsCopy[i]) {
+                    return true;
+                }
+                if (last != j) {
+                    if (numsCopy[j] == numsCopy[last - 1 < 0 ? last : last--]) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    // Accepted
+    public boolean containsDuplicateLoops2(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (i + 1 != nums.length) {
+                if (nums[i] == nums[i + 1]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    // TLE
+    public boolean containsDuplicateList(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (!list.contains(nums[i])) {
+                list.add(nums[i]);
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 2, 2, 4, 5};
+        int[] nums2 = new int[]{1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
+        int[] nums3 = new int[]{1, 2, 3, 4};
+        System.out.println(new ContainsDuplicate().containsDuplicateList(nums));
+        System.out.println(new ContainsDuplicate().containsDuplicateList(nums2));
+        System.out.println(new ContainsDuplicate().containsDuplicateList(nums3));
+
+        System.out.println();
+        System.out.println(new ContainsDuplicate().containsDuplicateLoops(nums));
+        System.out.println(new ContainsDuplicate().containsDuplicateLoops(nums2));
+        System.out.println(new ContainsDuplicate().containsDuplicateLoops(nums3));
+
+        System.out.println();
+        System.out.println(new ContainsDuplicate().containsDuplicateLoops2(nums));
+        System.out.println(new ContainsDuplicate().containsDuplicateLoops2(nums2));
+        System.out.println(new ContainsDuplicate().containsDuplicateLoops2(nums3));
+    }
+
+}

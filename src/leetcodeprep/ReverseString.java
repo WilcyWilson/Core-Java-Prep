a0e270c0c517384cea15ee41bb67af99550d0e64
wilcywilson <wilsonshresthaapps@gmail.com>
2026-08-23 17:20:51 +0545

    Reverse String Stack

--- src/leetcodeprep/ReverseString.java
+++ src/leetcodeprep/ReverseString.java
@@ -1,9 +1,10 @@
 package leetcodeprep;
 
+import java.util.Stack;
+
 public class ReverseString {
     public void reverseString(char[] s) {
-        int right = s.length - 1;
-        int left = 0;
+        int right = s.length - 1, left = 0;
         char temp;
         while (left < right) {
             temp = s[left];
@@ -15,8 +16,23 @@ public class ReverseString {
         System.out.println(s);
     }
 
+    public void reverseStringStack(char[] s) {
+        Stack<Character> characterStack = new Stack<>();
+        for (char c : s) {
+            characterStack.push(c);
+        }
+
+        // LIFO Reverses the string
+        for (int i = 0; i < s.length; i++) {
+            s[i] = characterStack.pop();
+        }
+        System.out.println(s);
+    }
+
     public static void main(String[] args) {
         new ReverseString().reverseString(new char[]{'H', 'e', 'l', 'l', 'o'});
         new ReverseString().reverseString(new char[]{'H', 'a', 'n', 'n', 'a', 'h'});
+        new ReverseString().reverseStringStack(new char[]{'H', 'e', 'l', 'l', 'o'});
+        new ReverseString().reverseStringStack(new char[]{'H', 'a', 'n', 'n', 'a', 'h'});
     }
 }

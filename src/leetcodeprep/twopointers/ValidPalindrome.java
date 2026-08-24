d3cf4a3435b5d398df2092d361ff40c73e552a7f
wilcywilson <wilsonshresthaapps@gmail.com>
2026-08-24 14:24:22 +0545

    Valid palindrome double while

--- src/leetcodeprep/twopointers/ValidPalindrome.java
+++ src/leetcodeprep/twopointers/ValidPalindrome.java
@@ -32,10 +32,64 @@ public class ValidPalindrome {
         return true;
     }
 
+    public boolean isPalindromeUsingCharacter(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) {
+            if (Character.isLetterOrDigit(s.charAt(left)) && Character.isLetterOrDigit(s.charAt(right))) {
+                if (Character.toLowerCase(s.charAt(left)) == Character.toLowerCase(s.charAt(right))) {
+                    left++;
+                    right--;
+                } else {
+                    return false;
+                }
+            } else {
+                if (!Character.isLetterOrDigit(s.charAt(left))) {
+                    left++;
+                }
+                if (!Character.isLetterOrDigit(s.charAt(right))) {
+                    right--;
+                }
+            }
+        }
+        return true;
+    }
+
+    // Double While loop doesn't automatically mean O(n^2) Time complexity. The inner while are just fast forwarding the same pointer values, not restarting from scratch
+    // Like with other nested loops
+    public boolean isPalindromeDoubleWhile(String s) {
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         System.out.println(new ValidPalindrome().isPalindrome("A man, a plan, a canal: Panama"));
         System.out.println(new ValidPalindrome().isPalindrome("race a car"));
         System.out.println(new ValidPalindrome().isPalindrome(" "));
 
+        System.out.println();
+        System.out.println(new ValidPalindrome().isPalindromeUsingCharacter("A man, a plan, a canal: Panama"));
+        System.out.println(new ValidPalindrome().isPalindromeUsingCharacter("race a car"));
+        System.out.println(new ValidPalindrome().isPalindromeUsingCharacter(" "));
+
+        System.out.println();
+        System.out.println(new ValidPalindrome().isPalindromeDoubleWhile("A man, a plan, a canal: Panama"));
+        System.out.println(new ValidPalindrome().isPalindromeDoubleWhile("race a car"));
+        System.out.println(new ValidPalindrome().isPalindromeDoubleWhile(" "));
     }
 }

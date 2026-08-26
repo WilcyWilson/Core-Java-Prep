0b5b9c2754649e8d151094e0767b3ef1fa368f12
wilcywilson <wilsonshresthaapps@gmail.com>
2026-08-26 23:09:33 +0545

    Squares of a Sorted Optimial

--- src/leetcodeprep/twopointers/SquaresOfASortedArray.java
+++ src/leetcodeprep/twopointers/SquaresOfASortedArray.java
@@ -18,7 +18,6 @@ public class SquaresOfASortedArray {
         for (int i = 0; i < nums.length; i++) {
             nums[i] = nums[i] * nums[i];
         }
-        System.out.println(Arrays.toString(nums));
         int left = 0;
         int right = nums.length - 1;
         for (int i = nums.length - 1; i >= 0; i--) {
@@ -33,9 +32,28 @@ public class SquaresOfASortedArray {
         return result;
     }
 
+    public int[] sortedSquaresOptimized(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        int left = 0;
+        int right = n - 1;
+        for (int i = n - 1; i >= 0; i--) {
+            if (Math.abs(nums[left]) > Math.abs(nums[right])) {
+                result[i] = nums[left] * nums[left];
+                left++;
+            } else {
+                result[i] = nums[right] * nums[right];
+                right--;
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         System.out.println(Arrays.toString(new SquaresOfASortedArray().sortedSquaresBruteForce(new int[]{-4, -1, 0, 3, 10})));
         System.out.println(Arrays.toString(new SquaresOfASortedArray().sortedSquaresLoops(new int[]{-4, -1, 0, 3, 10})));
-        System.out.println(Arrays.toString(new SquaresOfASortedArray().sortedSquaresLoops(new int[]{-7,-3,2,3,11})));
+        System.out.println(Arrays.toString(new SquaresOfASortedArray().sortedSquaresLoops(new int[]{-7, -3, 2, 3, 11})));
+        System.out.println(Arrays.toString(new SquaresOfASortedArray().sortedSquaresOptimized(new int[]{-7, -3, 2, 3, 11})));
+
     }
 }

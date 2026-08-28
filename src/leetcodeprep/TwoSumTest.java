b93c8bf14916b881ad08027ea281c02db804a1f1
wilcywilson <wilsonshresthaapps@gmail.com>
2026-08-28 19:46:55 +0545

    JUnit multiple test methods

--- src/leetcodeprep/TwoSumTest.java
+++ src/leetcodeprep/TwoSumTest.java
@@ -6,10 +6,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class TwoSumTest {
     @Test
-    public void testTwoSum() {
+    public void testTwoSum_ShouldFindSolution() {
         TwoSum t1 = new TwoSum();
         int[] result = t1.twoSumHashMap(new int[]{2, 7, 11, 15}, 9);
         assertArrayEquals(new int[]{0, 1}, result);
+    }
+
+    @Test
+    public void testTwoSum_ShouldResultInAnException(){
+        TwoSum t1 = new TwoSum();
         int [] array2 = new int[]{2, 7, 11, 15};
         int target2 = 15;
 

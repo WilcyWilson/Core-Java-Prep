ca24e4124465923871ed281e346965a9bb6a8f5c
wilcywilson <wilsonshresthaapps@gmail.com>
2026-09-07 14:16:25 +0545

    \s and \ in textblocks

--- src/javaprepfirstscope/TextBlocksDemo.java
+++ src/javaprepfirstscope/TextBlocksDemo.java
@@ -11,7 +11,7 @@ public class TextBlocksDemo {
         // With Text Blocks. Clean, Readable, Preserves text formating
         String json2 = """
                 {
-                "name": "Bruce",
+                "name": "Bruce",  \s
                 "age": 40,
                 "city": "Gotham"
                 }
@@ -24,5 +24,20 @@ public class TextBlocksDemo {
             """; //Moving the closing delimiter preserves the leading white spaces
         // Result: "    line 1\n    line 2"  (4 spaces preserved)
         System.out.println(indented);
+
+        // \s preserves trailing spaces which are normally stripped
+        // drag the mouse pointer to see the space in the output
+        String spaces = """
+                line1 \s
+                line2 \s
+                """;
+        System.out.println(spaces);
+
+        String longline = """
+                This is a very long line \
+                and all this line should appear in the same line without \
+                new line breaking these line.
+                """;
+        System.out.println(longline);
     }
 }

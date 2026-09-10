63a8dda013c501db08dcc347003e12acfe479683
wilcywilson <wilsonshresthaapps@gmail.com>
2026-09-10 20:20:42 +0545

    Virtual Threads

--- src/javaprepfirstscope/PlatformThreads.java
+++ src/javaprepfirstscope/PlatformThreads.java
@@ -2,10 +2,23 @@ package javaprepfirstscope;
 
 public class PlatformThreads {
     public static void main(String[] args) {
+        virtualThreadDemo();
+        // platformThreadDemo();
+    }
+
+    public static void platformThreadDemo() {
         for (int i = 0; i < 1_000_000; i++) { // Java 7 readability cosmetic 1_000_000
             new Thread(() ->
                     System.out.println("running thread")
             ).start();
+        }
+    }
+
+    public static void virtualThreadDemo() {
+        try (var executor = java.util.concurrent.Executors.newVirtualThreadPerTaskExecutor()) {
+            for (int i = 0; i < 1_000_000; i++) {
+                executor.submit(() -> System.out.println("running thread"));
+            }
         }
     }
 }

21cccbb2314663d912629ab516953732272aa7da
wilcywilson <wilsonshresthaapps@gmail.com>
2026-09-12 09:22:22 +0545

    Virtual Thread Options

--- src/javaprepfirstscope/PlatformThreads.java
+++ src/javaprepfirstscope/PlatformThreads.java
@@ -1,10 +1,14 @@
 package javaprepfirstscope;
 
+import java.util.concurrent.Executors;
+
 public class PlatformThreads {
     public static void main(String[] args) {
 //        virtualThreadDemo();
 //         platformThreadDemo();
         virtualThreadCreationOption1();
+        virtualThreadCreationOption2();
+        virtualThreadCreationOption3();
     }
 
     public static void platformThreadDemo() {
@@ -31,6 +35,33 @@ public class PlatformThreads {
             v.join(); // main waits until v finishes
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    // Thread.ofVirtual()
+    // Option 2
+    public static void virtualThreadCreationOption2() {
+        try {
+            Thread vthread = Thread
+                    .ofVirtual()
+                    .name("worker-", 0)
+                    .unstarted(() ->
+                            System.out.println("Hello from " + Thread
+                                    .currentThread()
+                                    .getName() + " " + Thread.currentThread()));
+            vthread.start();
+            vthread.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    //Option 3
+    public static void virtualThreadCreationOption3() {
+        try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
+            executor.submit(() ->
+                    System.out.println("Running on virtual thread " + Thread.currentThread()
+                    ));
         }
     }
 }

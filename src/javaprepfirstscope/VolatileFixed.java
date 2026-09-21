2912f0622e6686b4364c4e5be86c2019a8044ffd
wilcywilson <wilsonshresthaapps@gmail.com>
2026-09-21 13:46:18 +0545

    Volatile Happens Before

---
+++ src/javaprepfirstscope/VolatileFixed.java
@@ -0,0 +1,20 @@
+package javaprepfirstscope;
+
+public class VolatileFixed {
+    private static volatile boolean ready;
+    private static int number; // visibility now guaranteed
+
+    void main() throws InterruptedException {
+        new Thread(() -> {
+            // System.out.println(number); // Flipping the order hear will result in number = 0
+            while (!ready) { // volatile ready establishes happens before edge
+                Thread.yield();
+            }
+            System.out.println(number); // Inherits visibility by transitivity. Reading it after volatile ready
+        }).start();
+
+        Thread.sleep(100);
+        number = 42; // happens before ready = true
+        ready = true; // volatile write
+    }
+}
